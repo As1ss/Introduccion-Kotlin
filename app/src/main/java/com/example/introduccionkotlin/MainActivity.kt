e3229a726775fca41908bff039af4b27e8932307
As1ss <alexis.lopbri@educa.jcyl.es>
2023-06-16 20:13:58 +0200

    Realizado ejemplos sobre los tipos de datos y prueba teórica.

--- app/src/main/java/com/example/introduccionkotlin/MainActivity.kt
+++ app/src/main/java/com/example/introduccionkotlin/MainActivity.kt
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val moneda = "EUR"
+    }
+    var saldo: Float =360.54f
+    var sueldo  = 764.82
+    var cantidad: Int = 5
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fecha = "16/06/2023";
+        val fecha = "16/06/2023"
+        var nombre = "Alexis"
+        var vip: Boolean = false
+        var inicial: Char='A'
+        var saludo ="Hola "+nombre
 
 
-        var nombre = "Alexis";
-
-        println(nombre);
+        println(saludo)
     }
 }
