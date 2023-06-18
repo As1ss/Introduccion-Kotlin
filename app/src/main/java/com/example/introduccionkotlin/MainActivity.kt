84a5efa7146e2cd51de7bca4eb2736ae7c408f2c
As1ss <alexis.lopbri@educa.jcyl.es>
2023-06-18 15:52:38 +0200

    Ejemplos de operadores de comparación

--- app/src/main/java/com/example/introduccionkotlin/MainActivity.kt
+++ app/src/main/java/com/example/introduccionkotlin/MainActivity.kt
@@ -21,43 +21,52 @@ class MainActivity : AppCompatActivity() {
         var vip: Boolean = false
         var inicial: Char = 'A'
         var saludo = "Hola " + nombre
-        println(saludo)
+
+
 
         //Operadores aritméticos
+        /*
+                var a: Int = 5 + 5 //10
+                var b: Int = 10 - 2 //8
+                var c: Int = 3 * 4 //12
+                var d: Int = 10 / 5 //2
+                var e: Int = 10 % 3 //1
+                var f: Int =
+                    10 / 6 //1 , deberia ser decimal periodico pero como el tipo de dato es INT solo se queda con el número entero
 
-        var a: Int = 5 + 5 //10
-        var b: Int = 10 - 2 //8
-        var c: Int = 3 * 4 //12
-        var d: Int = 10 / 5 //2
-        var e: Int = 10 % 3 //1
-        var f: Int =
-            10 / 6 //1 , deberia ser decimal periodico pero como el tipo de dato es INT solo se queda con el número entero
-
-        var aPreIncremento: Int = 5
-        var bPreDecremento: Int = 5
-        var cPostIncremento: Int = 5
-        var cPostDecremento: Int = 5
-
-
-        println("Valor inicial: " + aPreIncremento) //5
-        println("Valor incrementado: " + ++aPreIncremento) //Incrementa en uno , salida 6
-        println("Valor final: " + aPreIncremento) //6
-
-        println("Valor inicial: " + bPreDecremento) //5
-        println("Valor decrementado: " + --bPreDecremento) //Decrementa en uno , salida 4
-        println("Valor final: " + bPreDecremento)
-
-        println("Valor inicial: " + cPostIncremento) //5
-        println("Valor al incrementar: " + cPostIncremento++) //Muestra el valor y después lo incrementa , salida 5
-        println("Valor final: " + cPostIncremento) //6
-        println("Valor inicial: " + cPostDecremento) //5
-        println("Valor al decrementar: " + cPostDecremento--) //Muestra el valor y después lo decrementa, salida 5
-        println("Valor final: " + cPostDecremento) //4
-
-        saldo += sueldo
-
-        saldo++
+                var aPreIncremento: Int = 5
+                var bPreDecremento: Int = 5
+                var cPostIncremento: Int = 5
+                var cPostDecremento: Int = 5
 
 
+                println("Valor inicial: " + aPreIncremento) //5
+                println("Valor incrementado: " + ++aPreIncremento) //Incrementa en uno , salida 6
+                println("Valor final: " + aPreIncremento) //6
+
+                println("Valor inicial: " + bPreDecremento) //5
+                println("Valor decrementado: " + --bPreDecremento) //Decrementa en uno , salida 4
+                println("Valor final: " + bPreDecremento)
+
+                println("Valor inicial: " + cPostIncremento) //5
+                println("Valor al incrementar: " + cPostIncremento++) //Muestra el valor y después lo incrementa , salida 5
+                println("Valor final: " + cPostIncremento) //6
+                println("Valor inicial: " + cPostDecremento) //5
+                println("Valor al decrementar: " + cPostDecremento--) //Muestra el valor y después lo decrementa, salida 5
+                println("Valor final: " + cPostDecremento) //4
+
+                saldo += sueldo
+
+                saldo++
+        */
+        //Operadores de comparación
+        /*
+                a == b
+                a != b
+                a > b
+                a < b
+                a >= b
+                a <= b
+        */
     }
 }
